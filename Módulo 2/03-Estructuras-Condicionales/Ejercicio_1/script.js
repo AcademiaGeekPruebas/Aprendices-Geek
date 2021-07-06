c57d3812cc616554c04191011f9169be3319280b
Jenny Alexandra Montoya Cortés <30633488+jennymontoya1001@users.noreply.github.com>
2021-07-06 10:46:53 -0500

    Update script.js

--- Módulo 2/03-Estructuras-Condicionales/Ejercicio_1/script.js
+++ Módulo 2/03-Estructuras-Condicionales/Ejercicio_1/script.js
@@ -1,26 +1,16 @@
-let cantidad = 0;
-let salario = 0;
-let extra = 0;
+let cap = 0;
+let p_int = 0;
+let capf = 0;
+let int = 0;
+
+cap = Number(prompt('Ingrese capital invertido'));
+p_int = Number(prompt('Ingrese concepto intereses'));
+
+int = cap * p_int;
+
+if(int > 7000){
+    capf = cap + int;
+    console.log('El capital final es: '+ capf);
+}
 
 
-
- function calcular(){
-
-    cantidad = document.getElementById('cantidad').value;
-    
-    if(cantidad>=1 && cantidad<=40){
-          salario = cantidad * 16000;
-          document.getElementById('salario').value = salario;
-          document.getElementById('salarioextra').value = 0;
-          document.getElementById('total').value = salario;
-
-    }else {
-           extra = cantidad - 40;
-           salario = (extra*20000) + (40*16000);
-           document.getElementById('salario').value = 40*16000;
-           document.getElementById('salarioextra').value = extra*20000;
-           document.getElementById('total').value = salario;       
-    }
-
-
- }
